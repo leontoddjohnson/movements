5f067b0af985d9696ade6caacaa80a76f6fe10be
Leon <airportpeoplemusic@gmail.com>
2024-10-27 13:00:21 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# dots
+
+> This script was built as a study in softcut and granular engines.
+
+Inspired by the granular tool in the torso s4.
+
+Constantly record input over some interval between 1-n seconds. This remains as a "static loop" which can be mixed in. Second buffer 
---
+++ dots.lua
@@ -0,0 +1,134 @@
+-- dots
+--
+-- See docs.
+
+n_samples = 64
+
+waveform = {}
+scale = 30
+
+positions = {0, 0, 0, 0}
+
+poll_time = 1
+amp_threshold = 0.01
+
+amp_l = 0
+amp_r = 0
+
+function init()
+  audio.level_adc_cut(1)
+
+  poll_amp_l = poll.set("amp_in_l", update_amp_l)
+  poll_amp_l.time = poll_time
+  poll_amp_l:start()
+
+  poll_amp_r = poll.set("amp_in_r", update_amp_r)
+  poll_amp_r.time = poll_time
+  poll_amp_r:start()
+
+  -- set bounce times
+  time = metro.init()
+  time.time = 0.5
+  time.event = move
+  time:start()
+
+  build_params()
+  sc_reset()
+  redraw()
+end
+
+function update_amp_l(a)
+  amp_l = a
+  sound = amp_l + amp_r >= amp_threshold
+end
+
+function update_amp_r(a)
+  amp_r = a
+  sound = amp_l + amp_r >= amp_threshold
+end
+
+function move()
+  if sound then
+    for i = 3,4 do
+      pos = math.random() * params:get('loop_length')
+      softcut.position(i, pos)
+      softcut.play(i, 1)
+    end
+  else
+    for i = 3,4 do
+      softcut.play(i, 0)
+      softcut.position(i, 0)
+    end
+  end
+  
+  redraw()
+end
+
+function build_params()
+
+  params:add_number('loop_length', 'loop_length', 0, 10, 3)
+
+end
+
+function sc_reset()
+  softcut.buffer_clear()
+
+  for i=1,4 do
+    -- init
+    softcut.enable(i, 1)
+    softcut.buffer(i, 1)
+    softcut.level(i, 1)
+    softcut.rate(i, 1)
+    softcut.loop(i, 1)
+    softcut.loop_start(i, 0)
+    softcut.loop_end(i, params:get('loop_length'))
+    softcut.position(i, 0)
+    softcut.play(i, 1)
+    softcut.fade_time(i, 0.1)
+    softcut.pan(i, i % 2 == 0 and 1 or -1)
+
+    -- watch position
+    softcut.phase_quant(i, 0.01)
+    softcut.event_phase(update_position)
+    softcut.poll_start_phase()
+
+    if i < 2 then
+      softcut.rec_level(i, 1)
+      softcut.pre_level(i, 0)
+      softcut.level_input_cut(i, i, 1)
+      softcut.rec(i, 1)
+      softcut.play(i, 1)
+    end
+  end
+end
+
+function update_position(i,pos)
+  positions[i] = pos
+  redraw()
+end
+
+-- position of voice i in terms of 1 to 100 pixels
+function position_to_pixels(i)
+  p = positions[i]
+  -- the line is 100 pixels long
+  return (p / params:get('loop_length')) * 100
+end
+
+function redraw()
+  screen.clear()
+
+  -- baseline
+  screen.move(14, 20)
+  screen.line(114, 20)
+
+  -- voice position (above or below line)
+  for i=1,4 do
+    lr = i % 2 == 0 and 1 or 0
+    screen.move(14 + position_to_pixels(i), 10 + 10 * lr)
+    screen.line_rel(0, 10)
+  end
+
+  screen.stroke()
+  screen.update()
+end
+
